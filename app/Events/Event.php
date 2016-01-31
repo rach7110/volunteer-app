99ea920d87b991121daadb7c9286895f595ec9bb
Rachel <rachel.loziuk@gmail.com>
2016-01-31 12:05:14 -0600

    define relationship between User and Event models

--- app/Events/Event.php
+++ app/Events/Event.php
@@ -4,5 +4,8 @@ namespace App\Events;
 
 abstract class Event
 {
-    //
+    public function user()
+    {
+    	return $this->belongsTo(User::class);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -23,4 +23,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public funtion events() 
+    {
+        return $this->hasMany(Events::class);
+    }
+
 }
