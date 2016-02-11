3b074bdd281f5600ee41e65a6cceddfd8ae0afe8
Rachel Loziuk <rachel.loziuk@gmail.com>
2016-02-11 10:07:17 -0600

    fix errors in User and Event models

--- app/User.php
+++ app/User.php
@@ -24,7 +24,7 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    public funtion events() 
+    public function events()
     {
         return $this->hasMany(Events::class);
     }
