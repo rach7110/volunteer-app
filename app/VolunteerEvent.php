28a656cda50595f0bbfaf293809803011a9d0652
Rachel <rachel.loziuk@gmail.com>
2016-02-27 15:57:03 -0600

    add protected attributes to model and name table

--- app/VolunteerEvent.php
+++ app/VolunteerEvent.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class VolunteerEvent extends Model
 {
+	protected $table = 'volunteerevents';
+	protected $fillable = ['title'];
+	
         public function user()
     {
     	return $this->belongsTo(User::class);
